bc209ddc7ab4f46740d99e77d200dc5da5d3ef97
anb1906423 <100342430+anb1906423@users.noreply.github.com>
2023-01-16 20:26:28 +0700

    cmt model testdrive

--- app/model/TestDrive.js
+++ app/model/TestDrive.js
@@ -1,28 +1,28 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema
 
-const testDriveSchema = new Schema({
-    fullName: {
-        type: String,
-        required: true
-    },
-    phoneNumber: {
-        type: String,
-        required: true
-    },
-    created: {
-        type: Date,
-        default: Date.now()
-    },
-    modelInterest: {
-        type: String,
-    },
-})
+// const testDriveSchema = new Schema({
+//     fullName: {
+//         type: String,
+//         required: true
+//     },
+//     phoneNumber: {
+//         type: String,
+//         required: true
+//     },
+//     created: {
+//         type: Date,
+//         default: Date.now()
+//     },
+//     modelInterest: {
+//         type: String,
+//     },
+// })
 
-testDriveSchema.method("toJSON", function () {
-    const { __v, _id, ...object } = this.toObject()
-    object.id = _id
-    return object
-})
+// testDriveSchema.method("toJSON", function () {
+//     const { __v, _id, ...object } = this.toObject()
+//     object.id = _id
+//     return object
+// })
 
-module.exports = mongoose.model('TestDrive', testDriveSchema)
+// module.exports = mongoose.model('TestDrive', testDriveSchema)
